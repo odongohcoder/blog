0927e39244f85ed70d0646f24776d785586207d6
Joep <joepdooper@hotmail.com>
2020-05-23 02:10:05 +0200

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -1,28 +1,3 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.7.29)
-# Datenbank: blog
-# Erstellt am: 2020-05-23 00:07:25 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Export von Tabelle comment
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `comment`;
 
 CREATE TABLE `comment` (
@@ -37,11 +12,6 @@ CREATE TABLE `comment` (
   CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`postid`) REFERENCES `post` (`id`),
   CONSTRAINT `comment_ibfk_3` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Export von Tabelle paragraph
-# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `paragraph`;
 
@@ -59,11 +29,6 @@ CREATE TABLE `paragraph` (
   CONSTRAINT `paragraph_ibfk_4` FOREIGN KEY (`postid`) REFERENCES `post` (`id`) ON UPDATE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Export von Tabelle post
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `post`;
 
 CREATE TABLE `post` (
@@ -80,11 +45,6 @@ CREATE TABLE `post` (
   CONSTRAINT `post_ibfk_3` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Export von Tabelle service
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `service`;
 
 CREATE TABLE `service` (
@@ -93,11 +53,6 @@ CREATE TABLE `service` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Export von Tabelle subject
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `subject`;
 
 CREATE TABLE `subject` (
@@ -105,11 +60,6 @@ CREATE TABLE `subject` (
   `subject` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Export von Tabelle users
-# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `users`;
 
@@ -121,13 +71,3 @@ CREATE TABLE `users` (
   `users_image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

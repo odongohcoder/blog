6fe5c11f187835ea24394b7b1e90d6f3e987fdcd
Joep <joepdooper@hotmail.com>
2020-07-11 03:12:41 +0200

    Update menu.php

--- template/jane/menu.php
+++ template/jane/menu.php
@@ -6,7 +6,7 @@ defined('_BASE') or die('Something went wrong');
 	<?php foreach($pages as $key=>$val):?>
 		<?php if(isset($_SESSION['email']) || $key == "admin"): ?>
 		<li <?php if( $val['link'] == _DIR ):?>class="active"<?php endif;?>>
-			<a href="<?php echo _BASE; print $val['link']; ?>">
+			<a href="<?php print _BASE . $val['link']; ?>">
 				<?php if(!empty($_SESSION['users_image']) && $key == "admin"): ?>
 					<div class="users-image" style="background-image:url(<?php print _BASE . 'img/users/' . $_SESSION['users_image']; ?>)"></div>
 				<?php else: ?>
